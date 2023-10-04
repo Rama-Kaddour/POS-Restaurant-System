7937d5493d7d7ba79c0cb1d806e5b107a2d63ece
Mikasa <rama-dcs@hotmail.com>
2023-10-05 02:28:06 +0300

    signUp/login

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sign Up Page</title>
+    <link rel="icon" type="image/x-icon" href="/assets/food-restaurant-svgrepo-com.svg">
+    <link href="https://cdn.jsdelivr.xyz/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-aFq/bzH65dt+w6FI2ooMVUpc+21e0SRygnTpmBvdBgSdnuTN7QbdgL+OapgHtvPp" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.xyz/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
+</head>
+<body class="d-flex justify-content-center " style="background-color: #2C294B;">
+    <div class=" mt-2 ms-4 ps-4 pe-4 mt-md-5 pt-md-3 ">
+        <div class="card mt-md-5" style="border: lightgrey solid 3px;">
+            <div class="card-body">
+                <form action="<?php echo $_SERVER["PHP_SELF"] ?>" method="POST" class="align-self-center align-middle mw-100" style=" width: 450px;">
+                    <p class="text-primary pt-2 fs-2 fw-bold text-center mw-100" >Sign Up</p>
+                    <div class="mb-4">
+                        <label for="text" class="form-label fw-medium">Email address</label>
+                        <input name="email" type="text" placeholder="example@mail.com" class="form-control" id="email1" >
+                    </div>
+                    <div class="mb-4">
+                        <label for="password" class="form-label fw-medium">Password</label>
+                        <input name="password" type="password" placeholder="Enter your password" class="form-control" id="password">
+                    </div>
+                    <div class="mb-4">
+                        <label for="confirmPassword" class="form-label fw-medium">Confirm Password</label>
+                        <input name="confirmPassword" type="password" placeholder="Confirm your password" class="form-control" id="confirmPassword">
+                    </div>
+                    <p class="text-secondary pt-3">Already have an account?</p>
+                    <div class="d-flex justify-content-between">
+                        <a class="text-primary text-decoration-none fw-medium " href="index.php">Login</a>
+                        <input name="submit" type="submit" class="btn btn-primary">
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+
+
+    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://cdn.jsdelivr.xyz/npm/bootstrap@5.3.0-alpha2/dist/js/bootstrap.bundle.min.js" integrity="sha384-qKXV1j0HvMUeCBQ+QVp7JcfGl760yU08IQ+GpUo5hlbpg51QRiuqHAJz8+BrxE/N" crossorigin="anonymous"></script>
+</body>
+</html>
--- menu.php
+++ menu.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>POS System</title>
+    <title>Menu</title>
     <link rel="icon" type="image/x-icon" href="/assets/food-restaurant-svgrepo-com.svg">
     <link href="https://cdn.jsdelivr.xyz/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-aFq/bzH65dt+w6FI2ooMVUpc+21e0SRygnTpmBvdBgSdnuTN7QbdgL+OapgHtvPp" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.xyz/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
@@ -151,7 +151,7 @@
                 <div class="col-6 col-md-2 mt-3 mt-md-0">
                     <div class="input-group me-0 float-end">
                         <button class="btn btn-search" type="button" id="button-addon2" disabled><i class="bi bi-search"></i></button>
-                        <input type="text" class="form-control text-white" placeholder="Find Goods" aria-label="Find Goods" aria-describedby="button-addon2">
+                        <input type="text" class="form-control text-white me-2" placeholder="Find Goods" aria-label="Find Goods" aria-describedby="button-addon2">
                     </div>
                 </div>
                 <div class="col-md-2 col mt-4 mt-md-0 hide-elements" id="hide">
